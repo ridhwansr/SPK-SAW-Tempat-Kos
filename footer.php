dd28578eb1bcdae60962b71a4fb41351d9e1ea81
ridhwansr <153291689+ridhwansr@users.noreply.github.com>
2024-06-25 20:04:19 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="text-center bg-dark text-white py-3">
-    copyright $copy; 2024. Sistem Pendukung Keputusan Pemilihan Tempat Kos
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-
-</html>
